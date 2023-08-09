452c75b523ce79db30dc2dc3c59258d92862289e
m3xan1k <m3xan1k@duck.com>
2023-08-09 21:35:17 +0300

    lsp, evil

---
+++ elpa/evil-20230809.923/evil-pkg.el
@@ -0,0 +1,12 @@
+(define-package "evil" "20230809.923" "Extensible Vi layer for Emacs."
+  '((emacs "24.1")
+    (goto-chg "1.6")
+    (cl-lib "0.5"))
+  :commit "26db9441a13ebedb2481d7ada4c3b5e60ec22795" :maintainer
+  '("Tom Dalziel" . "tom.dalziel@gmail.com")
+  :keywords
+  '("emulations")
+  :url "https://github.com/emacs-evil/evil")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- init.el
+++ init.el
@@ -57,6 +57,9 @@
 ;; dont close emacs
 (setq confirm-kill-emacs 'yes-or-no-p)
 
+;; supress warnings
+(setq warning-minimum-level :error)
+
 ;; themes
 (use-package almost-mono-themes
   :ensure t
@@ -66,3 +69,83 @@
   (load-theme 'almost-mono-cream t)
   ;; (load-theme 'almost-mono-white t)
 )
+
+;; (use-package tao-theme
+;;   :init
+;;   (setq tao-theme-use-sepia t)
+;;   (setq tao-theme-sepia-depth 5)
+;;   (setq tao-theme-sepia-saturation 1)
+;;   (setq tao-theme-use-boxes nil)
+;;   :config
+;;   (load-theme 'tao-yang t))
+
+(use-package evil
+  :config
+  (evil-mode 1)
+  (evil-define-key 'normal lsp-mode-map (kbd "SPC l") lsp-command-map))
+
+
+(use-package good-scroll
+  :config
+  (good-scroll-mode 1))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;      tools             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;       lsp stuff        ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Company mode
+(use-package company
+  :config
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-Length 1)
+  :bind (:map company-active-map
+          ("C-n" . company-select-next)
+          ("C-p" . company-select-previous)))
+
+;; Lsp mode
+(use-package lsp-mode
+  :hook
+  (lsp-mode . lsp-enable-which-key-integration)
+  :config
+  (setenv "PATH" (concat
+                   "/usr/local/bin" path-separator
+                   (getenv "PATH")))
+  (dolist (m '(clojure-mode
+               clojurec-mode
+               clojurescript-mode
+               clojurex-mode))
+     (add-to-list 'lsp-language-id-configuration `(,m . "clojure"))))
+
+;; lsp-ui
+(use-package lsp-ui
+  :commands lsp-ui-mode)
+
+;; Python
+(use-package lsp-pyright
+  :defer t
+  :config
+  (setq lsp-pyright-disable-language-service nil
+	lsp-pyright-diagnostic-mode "openFilesOnly"
+	lsp-pyright-typechecking-mode "basic"
+	lsp-pyright-auto-search-paths t
+	lsp-pyright-use-library-code-for-types t
+	lsp-headerline-breadcrumb-mode t
+    ;; lsp-pyright-stub-path (concat (getenv "HOME") "/Documents/projects/python-type-stubs")
+    )
+
+  :hook ((python-mode . (lambda ()
+                          (require 'lsp-pyright)
+                          (lsp-deferred)))))
+
+;; Clojure
+;; M-x lsp-install-server RET clojure-lsp RET
+(use-package clojure-mode)
+(use-package cider)
