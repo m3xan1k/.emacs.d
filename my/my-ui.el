dc4f2f020d468f621f5ec9ce4afb6dd409ee1e16
m3xan1k <m3xan1k@duck.com>
2023-10-10 17:31:39 +0300

    doom line and berrys theme

--- my/my-ui.el
+++ my/my-ui.el
@@ -1,18 +1,10 @@
 ;; font
 (set-face-attribute 'default nil :font "Input" :height 170)
 
-;; themes
-;; (use-package almost-mono-themes
-;;   :ensure t
-;;   :config
-;;   (load-theme 'almost-mono-cream t))
-
-(use-package base16-theme
+(use-package berrys-theme
   :ensure t
-  :init
-  (setq base16-theme-256-color-source "colors")
   :config
-  (load-theme 'base16-grayscale-light t))
+  (load-theme 'berrys t))
 
 ;; tab bar
 (use-package centaur-tabs
@@ -29,7 +21,7 @@
 (add-hook 'dired-mode-hook 'centaur-tabs-local-mode)
 
 ;; The maximum displayed length of the branch name of version control.
-(setq modeline-vcs-max-length 32)
+(setq doom-modeline-vcs-max-length 32)
 
 ;; modeline customization
 (defun my/format-git-diff (plus-minus)
@@ -45,7 +37,7 @@
 
 (defun my/glue-branch-diff (branch-name plus-minus-formatted)
   "returns branch-name[diff]"
-  (let ((cut-length (- modeline-vcs-max-length
+  (let ((cut-length (- doom-modeline-vcs-max-length
 		       (+ 2 (length plus-minus-formatted)))))
     (if (< cut-length (length branch-name))
 	(concat (substring branch-name 0 cut-length)
@@ -60,27 +52,32 @@
     (setq ad-return-value
 	  (my/glue-branch-diff ad-return-value plus-minus-formatted))))
 
-(setq evil-normal-state-tag   (propertize "|NORMAL|" 'face '((:background "green" :foreground "black" :weight bold)))
-      evil-emacs-state-tag    (propertize "|EMACS|" 'face '((:background "black" :foreground "white" :weight bold)))
-      evil-insert-state-tag   (propertize "|INSERT|" 'face '((:background "red") :foreground "white" :weight bold))
-      evil-motion-state-tag   (propertize "|MOTION|" 'face '((:background "blue") :foreground "white" :weight bold))
-      evil-visual-state-tag   (propertize "|VISUAL|" 'face '((:background "yellow" :foreground "black" :weight bold)))
-      evil-operator-state-tag (propertize "|OPERATOR|" 'face '((:background "purple" :weight bold))))
+(use-package doom-modeline
+  :ensure t
+  :init
+  (doom-modeline-mode 1))
 
-(setq-default mode-line-format
-	      '("%e"
-		" "
-		(:eval
-		 (when (mode-line-window-selected-p)
-		 (format "%s" evil-mode-line-tag)))
-		" "
-		(:eval (format "%s" (buffer-name)))
-		" "
-		(:eval (format "%s" (symbol-name major-mode)))
-		" "
-		(format "%s" flymake-mode-line-counters)
-		(vc-mode vc-mode)
-		" "
-		"%l:%c"))
+;; (setq evil-normal-state-tag   (propertize "|NORMAL|" 'face '((:background "green" :foreground "black" :weight bold)))
+;;       evil-emacs-state-tag    (propertize "|EMACS|" 'face '((:background "black" :foreground "white" :weight bold)))
+;;       evil-insert-state-tag   (propertize "|INSERT|" 'face '((:background "red") :foreground "white" :weight bold))
+;;       evil-motion-state-tag   (propertize "|MOTION|" 'face '((:background "blue") :foreground "white" :weight bold))
+;;       evil-visual-state-tag   (propertize "|VISUAL|" 'face '((:background "yellow" :foreground "black" :weight bold)))
+;;       evil-operator-state-tag (propertize "|OPERATOR|" 'face '((:background "purple" :weight bold))))
+
+;; (setq-default mode-line-format
+;; 	      '("%e"
+;; 		" "
+;; 		(:eval
+;; 		 (when (mode-line-window-selected-p)
+;; 		 (format "%s" evil-mode-line-tag)))
+;; 		" "
+;; 		(:eval (format "%s" (buffer-name)))
+;; 		" "
+;; 		(:eval (format "%s" (symbol-name major-mode)))
+;; 		" "
+;; 		(format "%s" flymake-mode-line-counters)
+;; 		(vc-mode vc-mode)
+;; 		" "
+;; 		"%l:%c"))
 
 (provide 'my-ui)
