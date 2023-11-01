fd65c36dce48f541d290a1a54c81739791c0792a
m3xan1k <m3xan1k@duck.com>
2023-11-01 13:32:32 +0300

    modeline, reduce some ui stuff

--- my/custom.el
+++ my/custom.el
@@ -23,6 +23,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mode-line ((t (:family "Input" :height 0.8 :background "white"))))
- '(tab-line ((t (:height 0.9 :family "Input"))))
+ '(mode-line ((t (:family "Ricty Diminished" :height 0.8 :background "gray"))))
  '(trailing-whitespace ((t (:background "red1")))))
--- my/my-ui.el
+++ my/my-ui.el
@@ -1,34 +1,21 @@
 ;; font
-(set-face-attribute 'default nil :font "Input" :height 180)
+(set-face-attribute 'default nil :font "Ricty Diminished" :height 220)
 
+;; theme
 (use-package berrys-theme
   :ensure t
   :config
   (load-theme 'berrys t))
 
-;; tab bar
-;; (use-package centaur-tabs
-;;   :demand
-;;   :config
-;;   (centaur-tabs-mode t)
-;;   (centaur-tabs-group-by-projectile-project)
-;;   (centaur-tabs-change-fonts "Input" 120))
-
-;; (setq centaur-tabs-cycle-scope 'tabs)
-;; (setq centaur-tabs-set-modified-marker t)
-
-;; projectile for tabs grouping
+;; projectile for projects discovery
 (use-package projectile
   :init
   (projectile-mode t))
 
-;; disable in dired
-;; (add-hook 'dired-mode-hook 'centaur-tabs-local-mode)
-
 ;; The maximum displayed length of the branch name of version control.
-(setq doom-modeline-vcs-max-length 32)
+(setq modeline-vcs-max-length 32)
 
-;; modeline customization
+;; git status customization
 (defun my/format-git-diff (plus-minus)
   "Takes 1\t2 returns [+1-2]"
   (concat "["
@@ -42,7 +29,7 @@
 
 (defun my/glue-branch-diff (branch-name plus-minus-formatted)
   "returns branch-name[diff]"
-  (let ((cut-length (- doom-modeline-vcs-max-length
+  (let ((cut-length (- modeline-vcs-max-length
 		       (+ 2 (length plus-minus-formatted)))))
     (if (< cut-length (length branch-name))
 	(concat (substring branch-name 0 cut-length)
@@ -57,32 +44,14 @@
     (setq ad-return-value
 	  (my/glue-branch-diff ad-return-value plus-minus-formatted))))
 
-(use-package doom-modeline
-  :ensure t
-  :init
-  (doom-modeline-mode 1))
+;; hide minor modes
+(use-package minions
+  :config
+  (minions-mode t))
 
-;; (setq evil-normal-state-tag   (propertize "|NORMAL|" 'face '((:background "green" :foreground "black" :weight bold)))
-;;       evil-emacs-state-tag    (propertize "|EMACS|" 'face '((:background "black" :foreground "white" :weight bold)))
-;;       evil-insert-state-tag   (propertize "|INSERT|" 'face '((:background "red") :foreground "white" :weight bold))
-;;       evil-motion-state-tag   (propertize "|MOTION|" 'face '((:background "blue") :foreground "white" :weight bold))
-;;       evil-visual-state-tag   (propertize "|VISUAL|" 'face '((:background "yellow" :foreground "black" :weight bold)))
-;;       evil-operator-state-tag (propertize "|OPERATOR|" 'face '((:background "purple" :weight bold))))
-
-;; (setq-default mode-line-format
-;; 	      '("%e"
-;; 		" "
-;; 		(:eval
-;; 		 (when (mode-line-window-selected-p)
-;; 		 (format "%s" evil-mode-line-tag)))
-;; 		" "
-;; 		(:eval (format "%s" (buffer-name)))
-;; 		" "
-;; 		(:eval (format "%s" (symbol-name major-mode)))
-;; 		" "
-;; 		(format "%s" flymake-mode-line-counters)
-;; 		(vc-mode vc-mode)
-;; 		" "
-;; 		"%l:%c"))
+;; minimal modeline
+(use-package mood-line
+  :config
+  (mood-line-mode))
 
 (provide 'my-ui)
